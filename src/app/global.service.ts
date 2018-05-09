7464e40fe09ca45ff782e3a1a942bfe28a51bac0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-09 18:47:02 +0000

    Deploy YZU-DL-Lab/font-to-font-web-interface to github.com/YZU-DL-Lab/font-to-font-web-interface.git:gh-pages

--- src/app/global.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class GlobalService {
-  constructor() { }
-  serverUrl = 'http://140.138.155.230:5000';
-}
